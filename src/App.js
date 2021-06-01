07e3d53d7fc6c2881768cde25adaa0fa4b066940
Noemi Czuczy <noemi.czuczy@gmail.com>
2021-06-01 09:58:12 +0200

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import {Component} from "react/cjs/react.production.min";
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
+    );
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +36,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;

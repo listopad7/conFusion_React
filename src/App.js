c7ed4ced4dbd20f264768f98a56ca9c9265ade1f
Noemi Czuczy <noemi.czuczy@gmail.com>
2021-06-01 10:36:11 +0200

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {Component} from "react/cjs/react.production.min";
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import {Component} from "react/cjs/react.production.min";
+import Menu from './components/MenuComponent';
+
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
+            <Menu />
         </div>
     );
   }

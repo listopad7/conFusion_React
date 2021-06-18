0785bb7a263bf06f192be1f11186008b5172ba8d
Noemi Czuczy <noemi.czuczy@gmail.com>
2021-06-18 09:56:22 +0200

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -54,7 +55,8 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={() =><About leaders={this.state.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
 
